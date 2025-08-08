ec8ae31c4a6979f8d3223b7574d43dc365e8d987
Musanef Yasser Macalatas <musanef93@gmail.com>
2025-08-08 13:27:23 +0800

    Add files via upload

---
+++ Mad-Libs-Game.py
@@ -0,0 +1,13 @@
+power = input("Enter power: ")
+thing = input("Enter a thing: ")
+verb = input("Enter a verb: ")
+place = input("Enter a place: ")
+animal = input("Enter an animal: ")
+
+# Create the mad libs story using the user's input
+story = f"One day, I saw a very {power} {animal} at the {place}. \
+It was trying to {verb} a {thing}, and everyone started laughing!"
+
+# Print the final story
+print("\n")
+print(story)
